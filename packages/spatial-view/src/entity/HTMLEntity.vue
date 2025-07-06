4376ef8789fa33324b046c859f3cf87a83f9347c
Toby Milner-Gulland <toby@figure.land>
2025-07-07 00:30:09 +0100

    feat: removed unnecessary icon

--- packages/spatial-view/src/entity/HTMLEntity.vue
+++ packages/spatial-view/src/entity/HTMLEntity.vue
@@ -88,12 +88,6 @@ const handleWheel = (event: WheelEvent) => {
         :editable="isEditing && isEditable" @cancel="handleCancel" />
     </div>
 
-    <!-- Visual indicator for read-only entities -->
-    <div v-if="!isEditable" class="read-only-indicator">
-      <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" fill="currentColor"/>
-      </svg>
-    </div>
   </div>
 </template>
 
