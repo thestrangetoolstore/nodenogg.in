8baf9cc1be3303f551c030c40fd9acbaf4e51793
Adam Procter <adam.procter@soton.ac.uk>
2025-07-15 22:00:32 +0100

    feat: added basic version number to footer
    edited the env.d.ts file in the nodenogg.in app to grab version number and display in <Footer>

--- products/app/src/components/footer/Footer.vue
+++ products/app/src/components/footer/Footer.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+const version = import.meta.env.VITE_APP_VERSION;
 </script>
 
 <template>
     <footer >
         <div class="footer-info">
             <p>Made by the University of Southampton, Winchester School of Art.</p>
+        <p>v{{ version }}</p>
         </div>
     </footer>
 </template>
