a61a1fe408c7085df83d2a3cf5ea1eb9101339c0
Adam Procter <adam.procter@soton.ac.uk>
2025-07-15 16:23:16 +0100

    feat: added basic footer
    add basic footer component to appear across the app

---
+++ products/app/src/components/footer/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <footer >
+        <div class="footer-info">
+            <p>Made by the University of Southampton, Winchester School of Art.</p>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
