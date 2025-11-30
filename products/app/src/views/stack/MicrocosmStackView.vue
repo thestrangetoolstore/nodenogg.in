a874c534dc978cb7a82f8aeb88bd8a9aac98b177
Adam Procter <adam.procter@soton.ac.uk>
2025-11-30 14:11:37 +0000

    fix: css changes only for microcosm grid

--- products/app/src/views/stack/MicrocosmStackView.vue
+++ products/app/src/views/stack/MicrocosmStackView.vue
@@ -204,11 +204,6 @@ const handleDuplicateEntity = async (e: Entity) => {
   left: 0;
   overflow-x: auto;
   overflow-y: hidden;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  padding: var(--size-12);
 }
 
 .columns-container {
